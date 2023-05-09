92bfe1aeb8e114337915d73d0161faccf680e752
BetNares <btari.f.r@mail.ugm.ac.id>
2023-05-09 18:11:24 +0700

    Connect frontend to backend
    (sekarang semuanya jalan di port 5000 dan gak perlu njalanin flask dan react secara terpisah)

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,6 +1,6 @@
 import {Container} from "@mui/material";
 
 function Login(){
-  return <Container>Hello World</Container>
+  return <Container>Hello World lalalalalala</Container>
 }
 export default Login;
