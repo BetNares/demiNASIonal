c1e30dc65ce9d3a60b6762c36ba81272a63531b6
BetNares <btari.f.r@mail.ugm.ac.id>
2023-05-23 15:49:22 +0700

    loading the model into app and make api

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template
 from reverseProxy import proxyRequest
+from flask import Flask, render_template, request
+import classifier
 
 MODE = os.getenv('FLASK_ENV')
 DEV_SERVER_URL = 'http://localhost:3000/'
@@ -17,4 +19,14 @@ def index(path=''):
     if MODE == 'development':
         return proxyRequest(DEV_SERVER_URL, path)
     else:
-        return render_template("index.html")    
+        return render_template("index.html")   
+
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    if (request.files['image']): 
+        file = request.files['image']
+
+        result = classifier.classifyImage(file)
+        print('Model classification: ' + result)        
+        return result 
